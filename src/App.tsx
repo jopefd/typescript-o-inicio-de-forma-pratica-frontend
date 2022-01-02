1378580117be0de527bb752ee9dc1beb3f005eaa
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-02 16:55:17 -0300

    Implement App with useEffect and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import api from './services/api'
+
+interface IUser {
+  name: string
+  email: string
+}
 
 function App() {
+  const [users, setUsers] = useState<Array<IUser>>([])
+
+  useEffect(() => {
+    api.get<Array<IUser>>('/users').then(res => {
+      setUsers(res.data)
+    })
+  })
+
   return (
     <div className="App">
       
     </div>
-  );
+  )
 }
 
 export default App;
